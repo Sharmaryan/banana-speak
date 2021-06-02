aaca230a2a7877250474c3bc22bae7ff0b9a5b24
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2021-06-02 20:14:26 +0530

    updated index.html

---
+++ app.js
@@ -0,0 +1,5 @@
+var btnTranslate = document.querySelector("#btn-translator");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+function clickHandler() {}
+btnTranslate.addEventListener("click", clickHandler);
